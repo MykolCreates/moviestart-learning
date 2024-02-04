6ab05c65e5e84b52869cf7bfbc36c66dd0e356b6
MykolCreates <mykol.creatives@gmail.com>
2024-02-04 16:33:56 +0800

    added Home and Favorites Pages

---
+++ app/(drawer)/(favorites)/_layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from 'expo-router';
+import Drawer from 'expo-router/drawer';
+import { Text, View } from 'react-native';
+
+const Layout = () => {
+  return (
+    <Stack>
+        {/* <Stack.Screen */}
+
+    </Stack>
+  );
+};
+
+export default Layout;
---
+++ app/(drawer)/_layout.tsx
@@ -0,0 +1,36 @@
+import { Ionicons } from '@expo/vector-icons';
+import { colorTokens } from '@tamagui/themes';
+import Drawer from 'expo-router/drawer';
+import { Text, View } from 'react-native';
+
+const Layout = () => {
+  return (
+    <Drawer
+      screenOptions={{
+        headerShown: true,
+        drawerHideStatusBarOnOpen: true,
+        drawerActiveBackgroundColor: colorTokens.dark.blue.blue7,
+        drawerActiveTintColor: '#fff',
+        drawerLabelStyle: { marginLeft: -20 },
+      }}>
+      <Drawer.Screen
+        name="(home)"
+        options={{
+          title: 'Moviestar',
+          headerShown: false,
+          drawerIcon: ({ color, size }) => <Ionicons name="home" size={size} color={color} />,
+        }}
+      />
+      <Drawer.Screen
+        name="(favorites)"
+        options={{
+          title: 'Favorites',
+          headerShown: false,
+          drawerIcon: ({ color, size }) => <Ionicons name="star" size={size} color={color} />,
+        }}
+      />
+    </Drawer>
+  );
+};
+
+export default Layout; 
